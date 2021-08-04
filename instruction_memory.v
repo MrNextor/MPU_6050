69dbee00405a4d104fb676991ff54481a2b39228
Vitaly Kondratenko <mr.nextor@gmail.com>
2021-08-04 20:43:59 +0300

    Oprom replaced by dprom

--- instruction_memory.v
+++ instruction_memory.v
@@ -1,38 +1,27 @@
-    `define CHECK     4'b0001 // check chip id MPU_6050
-    `define TMP_MSR   4'b0010 // measurement temperature
-    `define ACCEL_MSR 4'b0011 // accelerometer Measurements 
-    `define GYRO_MSR  4'b0100 // gyroscope measurements 
-    `define FIFO_EN   4'b0101 // FIFO Enable for temp, gyro and accel
-    `define FIFO_CNT  4'b0110 // keep track of the number of samples currently in the FIFO buffer    
-
-// commands for MPU_6050
-//
-// addr rom `CHECK - reg addr 8'h75 - chip-id : This value is fixed in MPU_6050 to 0x68 and can be used to check whether communication is functioning
-// addr rom `TMP_MSR - reg addr 8'h41
-// addr rom `ACCEL_MSR - reg addr 8'h3B
-// addr rom `GYRO_MSR - reg addr 8'h43
-// addr rom `FIFO_EN - by reg addr 8'h23 write 8'hF8
-// addr rom `FIFO_CNT - reg addr 8'h72
-
 module instruction_memory
-    #(parameter ADDR_ROM_SZ = 4,              // addr width in ROM 
-      parameter DATA_ROM_SZ = 8)              // word width in ROM
-    (CLK, I_ADDR_ROM, O_ADDR_ROM, O_DATA_ROM);
+    #(parameter ADDR_ROM_SZ = 4,  // addr width in ROM 
+      parameter DATA_ROM_SZ = 16) // word width in ROM
+    (CLK, I_ADDR_ROM_A, I_ADDR_ROM_B, 
+     O_DATA_ROM_A, O_DATA_ROM_B, O_ADDR_ROM_A, O_ADDR_ROM_B);
     
     
 //  input signals    
     input wire                    CLK;        // clock 50 MHz
-    input wire [ADDR_ROM_SZ-1:0]  I_ADDR_ROM; // word addr in ROM
+    input wire [ADDR_ROM_SZ-1:0]  I_ADDR_ROM_A; // word A addr in ROM
+    input wire [ADDR_ROM_SZ-1:0]  I_ADDR_ROM_B; // word B addr in ROM    
 //  output signals    
-    output reg [DATA_ROM_SZ-1:0]  O_DATA_ROM; // word in ROM
-    output wire [ADDR_ROM_SZ-1:0] O_ADDR_ROM; // word addr in ROM
+    output reg [DATA_ROM_SZ-1:0]  O_DATA_ROM_A; // word A in ROM
+    output reg [DATA_ROM_SZ-1:0]  O_DATA_ROM_B; // word B in ROM    
+    output wire [ADDR_ROM_SZ-1:0] O_ADDR_ROM_A; // word A addr in ROM
+    output wire [ADDR_ROM_SZ-1:0] O_ADDR_ROM_B; // word B addr in ROM    
 //  internal signals
     reg [DATA_ROM_SZ-1:0] rom_array [0:2**ADDR_ROM_SZ-1]; // ROM array
-    reg [ADDR_ROM_SZ-1:0] addr_reg; 
-    
+    reg [ADDR_ROM_SZ-1:0] addr_reg_a;
+    reg [ADDR_ROM_SZ-1:0] addr_reg_b; 
     
 //-------------------------------------------------------------------------- 
-    assign O_ADDR_ROM = addr_reg;
+    assign O_ADDR_ROM_A = addr_reg_a;
+    assign O_ADDR_ROM_B = addr_reg_b;
     
 //  read ROM content from file
     initial begin
@@ -41,8 +30,10 @@ module instruction_memory
 
 //  read operation  
     always @(posedge CLK) begin
-      addr_reg   <= I_ADDR_ROM;
-      O_DATA_ROM <= rom_array[I_ADDR_ROM];
+      addr_reg_a   <= I_ADDR_ROM_A;
+      addr_reg_b   <= I_ADDR_ROM_B;
+      O_DATA_ROM_A <= rom_array[I_ADDR_ROM_A];
+      O_DATA_ROM_B <= rom_array[I_ADDR_ROM_B];
     end
     
     
