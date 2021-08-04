77bae0709d5c87b83dd0f10e8b150c88706ac077
Vitaly Kondratenko <mr.nextor@gmail.com>
2021-08-04 21:02:48 +0300

    Updates comments

--- i2c_clk_div.v
+++ i2c_clk_div.v
@@ -1,13 +1,14 @@
 module i2c_clk_div
-    #(parameter FPGA_CLK = 50_000_000,             // FPGA frequency 50 MHz
-      parameter I2C_CLK  = 100_000)                // I2C bus frequency 100 KHz                   
+    #(parameter FPGA_CLK = 50_000_000, // FPGA frequency 50 MHz
+      parameter I2C_CLK  = 100_000)    // I2C bus frequency 100 KHz                   
     (CLK, RST_n, 
      O_SCL, O_RS_PR_SCL, O_FL_PR_SCL);
     
     
+//--------------------------------------------------------------------------     
     localparam DIV_CLK     = (FPGA_CLK / I2C_CLK); // the I2C bus frequency
     localparam DIV_CLK_1_4 = DIV_CLK / 4;          // 1/4 of the I2C bus frequency
-    localparam DIV_WIDTH   = $clog2(DIV_CLK);      //  counter widht for div 
+    localparam DIV_WIDTH   = $clog2(DIV_CLK);      // counter widht for div 
 //  input signals    
     input wire          CLK;                       // clock 50 MHz
     input wire          RST_n;                     // asynchronous reset_n
@@ -18,10 +19,9 @@ module i2c_clk_div
 //  internal signals    
     reg [DIV_WIDTH-1:0] cnt_clk;                   // counter clock
     reg [DIV_WIDTH-1:0] nx_cnt_clk;                // next counter clock                 
-    wire                nx_o_scl;                  // serial clock from i2c_clk_div      
-    wire                nx_o_rs_pr_scl;            // rising edge next_o_prev_scl for sda
-    wire                nx_o_fl_pr_scl;            // falling edge next_o_prev_scl for sda
-
+    wire                nx_o_scl;                  // next serial clock from i2c_clk_div      
+    wire                nx_o_rs_pr_scl;            // next rising edge next_o_prev_scl for sda
+    wire                nx_o_fl_pr_scl;            // next falling edge next_o_prev_scl for sda
 
 //  determining output signals next state 
     assign nx_o_rs_pr_scl = (cnt_clk == DIV_CLK_1_4 - 1'b1) ? 1'b1 : 1'b0;
--- i2c_master.v
+++ i2c_master.v
@@ -1,14 +1,15 @@
 module i2c_master
-    #(parameter FPGA_CLK = 50_000_000,   // FPGA frequency 50 MHz
-      parameter I2C_CLK  = 100_000,      // I2C bus frequency 100 KHz
-      parameter ADDR_SZ  = 7,            // address widht
-      parameter COMM_SZ  = ADDR_SZ + 1,  // command widht
-      parameter DATA_SZ  = 8)            // data widht
+    #(parameter FPGA_CLK = 50_000_000,  // FPGA frequency 50 MHz
+      parameter I2C_CLK  = 100_000,     // I2C bus frequency 100 KHz
+      parameter ADDR_SZ  = 7,           // address widht
+      parameter COMM_SZ  = ADDR_SZ + 1, // command widht
+      parameter DATA_SZ  = 8)           // data widht
     (CLK, RST_n, I_EN, I_ADDR, I_RW, I_DATA_WR, 
      O_DATA_RD, O_ACK_FL, O_BUSY, 
      IO_SCL, IO_SDA);
     
     
+//--------------------------------------------------------------------------    
 //  input signals
     input wire                CLK;       // clock 50 MHz
     input wire                RST_n;     // asynchronous reset_n
@@ -21,16 +22,15 @@ module i2c_master
     output wire               O_ACK_FL;  // flag in case of error
     output wire               O_BUSY;    // master busy signal
 //  bidirectional signals
-    inout wire                IO_SCL;    // serial clock I2C bus 
-    inout wire                IO_SDA;    // serial data I2C bus
+    inout wire IO_SCL; // serial clock I2C bus 
+    inout wire IO_SDA; // serial data I2C bus
 //  internal signals        
-    wire                      scl;       // serial clock from i2c_clk_div
-    wire                      rs_pr_scl; // rising edge o_prev_scl for sda
-    wire                      fl_pr_scl; // falling edge o_prev_scl for sda
-    wire                      sda_out;   // sda from I2C muster to slave
-    wire                      scl_out;   // scl from I2C muster to slave
-    wire                      sda;       // input sda from the slave
-
+    wire scl;       // serial clock from i2c_clk_div
+    wire rs_pr_scl; // rising edge o_prev_scl for sda
+    wire fl_pr_scl; // falling edge o_prev_scl for sda
+    wire sda_out;   // sda from I2C muster to slave
+    wire scl_out;   // scl from I2C muster to slave
+    wire sda;       // input sda from the slave
 
 //--------------------------------------------------------------------------
     i2c_clk_div 
